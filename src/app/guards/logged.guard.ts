c06a7a66c9de333985f886ff38b8fd60f64dfb2c
chino <luis.alberto.camarena@outlook.es>
2021-03-22 02:26:56 -0600

    implement security

---
+++ src/app/guards/logged.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggedGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(): boolean {
+    if (sessionStorage.getItem('information')) {
+      return true;
+    } else {
+      this.router.navigateByUrl('login');
+      return false;
+    }
+  }
+}
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedGuard } from '../guards/logged.guard';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [{
   path: '',
-  component: HomeComponent
+  component: HomeComponent,
+  canActivate: [LoggedGuard],
 }];
 
 @NgModule({
