49b67e6a8c8c622c99370ce76394cdd753c181a4
chino <luis.alberto.camarena@outlook.es>
2021-03-22 00:10:04 -0600

    modify service

--- src/app/services/get-employee/get-employee.service.ts
+++ src/app/services/get-employee/get-employee.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { EmployeeManagerModel } from 'src/app/models/employeeManager.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetEmployeeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getEmployee(): Observable<any> {
-    return of('https://run.mocky.io/v3/d5ddf1ff-a0e2-4a7e-bbcc-e832bef6a503');
+  getEmployee(): Observable<EmployeeManagerModel> {
+    const url = 'https://run.mocky.io/v3/d5ddf1ff-a0e2-4a7e-bbcc-e832bef6a503';
+    return this.http.get<EmployeeManagerModel>(url);
   }
 }
